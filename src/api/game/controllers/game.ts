95cfbebd126bf8b6217b5222a61c2f7d83350d67
Vinicius Amaral <viniciuscopuesb@gmail.com>
2024-04-05 23:29:03 -0300

    feat: add scrapper config

--- src/api/game/controllers/game.ts
+++ src/api/game/controllers/game.ts
@@ -2,6 +2,21 @@
  * game controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::game.game');
+export default factories.createCoreController(
+  "api::game.game",
+  ({ strapi }) => ({
+    async populate(ctx) {
+      const options = {
+        limit: 48,
+        order: "desc:trending",
+        ...ctx.query,
+      };
+
+      await strapi.service("api::game.game").populate(options);
+
+      ctx.send("Finished populating games!");
+    },
+  })
+);
